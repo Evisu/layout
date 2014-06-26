f622c9d8726c1f9325bdbbc8fed5748afd40739f
Evisu <sev7n.v@gmail.com>
2014-06-26 17:58:47 +0800

    2014.6.26 题型取自数据模型

---
+++ layoutit/problemTypes.js
@@ -0,0 +1,28 @@
+var problemTypes = [
+    {
+        problemTypeId:'0',
+        name:'单选题',
+        class:'basic-too11-icon-active',
+        problemType:'radio'
+    },
+    {
+        problemTypeId:'1',
+        name:'多选题',
+        class:'basic-too12-icon-active',
+        problemType:'checkbox'
+
+    },
+    {
+        problemTypeId:'2',
+        name:'填空题',
+        class:'basic-too13-icon-active',
+        problemType:'completion'
+
+    },
+    {
+        problemTypeId:'3',
+        name:'分页',
+        class:'basic-too13-icon-active',
+        problemType:'group'
+    }
+];
